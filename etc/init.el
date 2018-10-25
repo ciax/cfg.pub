560a877debfec3546d023433394acf70cc1ead6b
Omata@leonid <omata@naoj.org>
2018-10-25 13:15:42 -1000

    init.el: javascript indent 4 -> 2;

--- etc/init.el
+++ etc/init.el
@@ -15,3 +15,9 @@
   'executable-make-buffer-file-executable-if-script-p)
 ;; Includes
 ;;(require 'local_javascript)
+;;
+;; For Javascript (http://aimstogeek.hatenablog.com/entry/2016/10/25/214101)
+(add-hook 'js-mode-hook
+  (lambda ()
+    (make-local-variable 'js-indent-level)
+      (setq js-indent-level 2)))
